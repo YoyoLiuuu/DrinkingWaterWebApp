3e8d97300c01c8c0e5b6a8d51f1d8182394ca4c7
YoyoLiuuu <liuyimeng01@gmail.com>
2022-07-29 14:14:56 -0600

    login to main

--- home/urls.py
+++ home/urls.py
@@ -2,14 +2,14 @@
 # map view functions 
 
 from django.urls import path 
-from . import views 
+from . import home_views 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 #URLConf module 
 urlpatterns = [
-    path('', views.welcome),
-    path('hello/', views.say_hello) #just passing to this function, not passing anything yet
+    path('', home_views.welcome),
+    path('hello/', home_views.say_hello) #just passing to this function, not passing anything yet
 ]
 
 urlpatterns += staticfiles_urlpatterns()
